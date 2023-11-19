1181b3d89a7c2879bbf896260d8292a02f378b0c
Sebastian Torres <74284712+sebasdtc@users.noreply.github.com>
2023-11-19 02:46:43 -0500

    Implementacion del control SettingsExpander mediante ItemsControl (#1)
    ok =)

---
+++ FileOrganizer.Core/Contracts/Services/IDataAccessService.cs
@@ -0,0 +1,7 @@
+﻿using FileOrganizer.Core.Models;
+
+namespace FileOrganizer.Core.Contracts.Services;
+public interface IDataAccessService
+{
+    public Task<IEnumerable<Sample>> AllSample();
+}
---
+++ FileOrganizer.Core/Models/Sample.cs
@@ -0,0 +1,8 @@
+﻿namespace FileOrganizer.Core.Models;
+public class Sample
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+
+}
